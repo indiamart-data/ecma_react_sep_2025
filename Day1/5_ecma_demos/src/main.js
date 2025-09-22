abd55591be0b5392e5342f938197823b8aea2c8f
manish.sharma <manishvbn@gmail.com>
2025-09-22 17:56:05 +0530

    Fn Parameters

--- Day1/5_ecma_demos/src/main.js
+++ Day1/5_ecma_demos/src/main.js
@@ -7,6 +7,7 @@
 // import './1_datatypes/2_es6_declarations';
 // import './1_datatypes/3_es6_const';
 // import './1_datatypes/4_datatypes';
-import './1_datatypes/5_es6_symbol';
+// import './1_datatypes/5_es6_symbol';
 
 // import './2_functions/1_cr_fns'
+import './2_functions/2_fn_parameters'
