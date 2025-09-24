39941532fac992719689027ea38fe3ed4fb0241a
manish.sharma <manishvbn@gmail.com>
2025-09-24 11:30:04 +0530

    Multi Component Render

---
+++ Day3/2_react-core-app/src/components/2_multi-components/ComponentOne.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ComponentOne extends Component {
+    render() {
+        return (
+            <h1 className='text-primary'>
+                Hello from Component One
+            </h1>
+        );
+    }
+}
+
+export default ComponentOne;
---
+++ Day3/2_react-core-app/src/components/2_multi-components/ComponentTwo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ComponentTwo extends Component {
+    render() {
+        return (
+            <h1 className='text-success'>
+                Hello from Component Two
+            </h1>
+        );
+    }
+}
+
+export default ComponentTwo;
