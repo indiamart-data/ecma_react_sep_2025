3d8723e626afa73db5c794eca4a6eb8306bc1509
manish.sharma <manishvbn@gmail.com>
2025-09-25 17:51:23 +0530

    Error Handling

--- Day4/1_react-core-app/src/components/root/RootComponent.jsx
+++ Day4/1_react-core-app/src/components/root/RootComponent.jsx
@@ -3,16 +3,20 @@ import CounterWithReducer from '../2_reducers/CounterWithReducer';
 import CounterWithContext from '../3_context/CounterWithContext';
 import ControlledVsUncontrolledComponent from '../4_controlled-uncontrolled/ControlledVsUncontrolledComponent';
 import CalculatorAssignment from '../5_assignment/CalculatorAssignment';
+import ErrorHandler from '../common/ErrorHandler';
 
 const RootComponent = () => {
     return (
-        <div className='container'>
-            {/* <CounterAssignment /> */}
-            {/* <CounterWithReducer /> */}
-            {/* <CounterWithContext /> */}
-            {/* <ControlledVsUncontrolledComponent /> */}
-            <CalculatorAssignment />
-        </div>
+        <ErrorHandler>
+            <div className='container'>
+                {/* <CounterAssignment /> */}
+                {/* <CounterWithReducer /> */}
+                {/* <CounterWithContext /> */}
+                <ControlledVsUncontrolledComponent />
+                <hr />
+                <CalculatorAssignment />
+            </div>
+        </ErrorHandler>
     );
 };
 
