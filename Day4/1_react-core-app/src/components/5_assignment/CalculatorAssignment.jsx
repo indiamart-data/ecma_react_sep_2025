77deb00a28b569455a56681f201a7b42911db655
manish.sharma <manishvbn@gmail.com>
2025-09-25 16:33:09 +0530

    Composite UI

--- Day4/1_react-core-app/src/components/5_assignment/CalculatorAssignment.jsx
+++ Day4/1_react-core-app/src/components/5_assignment/CalculatorAssignment.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
+import TextInput from '../common/TextInput';
 
 class CalculatorOne extends Component {
     constructor(props) {
@@ -165,13 +166,129 @@ class CalculatorThree extends Component {
     }
 }
 
+class CalculatorFour extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { data: { t1: 0, t2: 0 }, result: 0 };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.reset = this.reset.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.setState({ result: parseInt(this.state.data.t1) + parseInt(this.state.data.t2) });
+    }
+
+    reset(e) {
+        this.setState({ data: { t1: 0, t2: 0 }, result: 0 });
+    }
+
+    handleChange(e) {
+        const field = e.target.id;
+        var obj = { ...this.state.data };
+        obj[field] = e.target.value;
+        this.setState({ data: obj });
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <div className="col-sm-6 offset-sm-3">
+                    <form className="justify-content-center">
+                        <fieldset>
+                            <legend className="text-center">Calculator Four - Composite UI</legend>
+                            <TextInput name="t1" label="Number One" placeholder="Enter Number One" value={this.state.data.t1}
+                                onChange={this.handleChange} />
+                            <TextInput name="t2" label="Number Two" placeholder="Enter Number Two" value={this.state.data.t2}
+                                onChange={this.handleChange} />
+                            <div className="form-group mb-2 mt-2">
+                                <h3>Result: {this.state.result}</h3>
+                            </div>
+                            <div className="d-grid gap-2 mx-auto">
+                                <button type="submit" className="btn btn-success" onClick={this.handleSubmit}>Add</button>
+                                <button type="reset" className="btn btn-primary" onClick={this.reset}>Reset</button>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+const CalculatorFive = () => {
+    const [data, setData] = useState({ t1: "0", t2: "0" });
+    const [result, setResult] = useState(0);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const n1 = parseFloat(data.t1) || 0;
+        const n2 = parseFloat(data.t2) || 0;
+        setResult(n1 + n2);
+    };
+
+    const reset = () => {
+        setData({ t1: "0", t2: "0" });
+        setResult(0);
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target; // prefer name over id
+        setData((prev) => ({ ...prev, [name]: value }));
+    };
+
+    return (
+        <div className="row">
+            <div className="col-sm-6 offset-sm-3">
+                <form className="justify-content-center" onSubmit={handleSubmit}>
+                    <fieldset>
+                        <legend className="text-center">Calculator Five - Composite UI</legend>
+
+                        <TextInput
+                            name="t1"
+                            label="Number One"
+                            placeholder="Enter Number One"
+                            value={data.t1}
+                            onChange={handleChange}
+                        />
+
+                        <TextInput
+                            name="t2"
+                            label="Number Two"
+                            placeholder="Enter Number Two"
+                            value={data.t2}
+                            onChange={handleChange}
+                        />
+
+                        <div className="form-group mb-2 mt-2">
+                            <h3>Result: {result}</h3>
+                        </div>
+
+                        <div className="d-grid gap-2 mx-auto">
+                            <button type="submit" className="btn btn-success">
+                                Add
+                            </button>
+                            <button type="button" className="btn btn-primary" onClick={reset}>
+                                Reset
+                            </button>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+    );
+};
+
 class CalculatorAssignment extends Component {
     render() {
         return (
             <div>
                 {/* <CalculatorOne /> */}
                 {/* <CalculatorTwo /> */}
-                <CalculatorThree />
+                {/* <CalculatorThree /> */}
+                {/* <CalculatorFour /> */}
+                <CalculatorFive />
             </div>
         );
     }
