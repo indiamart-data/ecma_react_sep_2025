4ac0c9241644222f7a56c0cfd21bd01fcb79fa5b
manish.sharma <manishvbn@gmail.com>
2025-09-26 15:24:30 +0530

    Crud Assignment Solution

--- Day5/1_react-core-app/src/components/root/RootComponent.jsx
+++ Day5/1_react-core-app/src/components/root/RootComponent.jsx
@@ -1,11 +1,13 @@
 import ListRoot from '../1_working_with_arrays/ListComponent';
+import CrudAssignment from '../2_assignment/CrudAssignment';
 import ErrorHandler from '../common/ErrorHandler';
 
 const RootComponent = () => {
     return (
         <ErrorHandler>
             <div className='container'>
-                <ListRoot />
+                {/* <ListRoot /> */}
+                <CrudAssignment />
             </div>
         </ErrorHandler>
     );
