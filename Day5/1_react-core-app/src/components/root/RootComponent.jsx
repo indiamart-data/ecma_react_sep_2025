11837b4d95ddf202c19302cdb6d1943ec20ecba4
manish.sharma <manishvbn@gmail.com>
2025-09-26 17:10:01 +0530

    Ajax Communication

--- Day5/1_react-core-app/src/components/root/RootComponent.jsx
+++ Day5/1_react-core-app/src/components/root/RootComponent.jsx
@@ -1,6 +1,7 @@
 import ListRoot from '../1_working_with_arrays/ListComponent';
 import CrudAssignment from '../2_assignment/CrudAssignment';
 import HookFormsDemo from '../3_hook-forms/HookFormsDemo';
+import AjaxComponent from '../4_ajax/AjaxComponent';
 import ErrorHandler from '../common/ErrorHandler';
 
 const RootComponent = () => {
@@ -8,8 +9,9 @@ const RootComponent = () => {
         <ErrorHandler>
             <div className='container'>
                 {/* <ListRoot /> */}
-                <CrudAssignment />
+                {/* <CrudAssignment /> */}
                 {/* <HookFormsDemo /> */}
+                <AjaxComponent />
             </div>
         </ErrorHandler>
     );
