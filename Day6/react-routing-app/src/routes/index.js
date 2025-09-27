b642b1afcd27d6ccee24935dbfc66ed606858fee
manish.sharma <manishvbn@gmail.com>
2025-09-27 11:26:18 +0530

    Nested Routing

--- Day6/react-routing-app/src/routes/index.js
+++ Day6/react-routing-app/src/routes/index.js
@@ -2,11 +2,27 @@ import { Route, Routes } from "react-router-dom";
 import HomeComponent from "../components/home/HomeComponent";
 import AboutComponent from "../components/about/AboutComponent";
 import NoMatchComponent from "../components/no-match/NoMatchComponent";
+import ProductsComponent from "../components/products/ProductsComponent";
+import AdminComponent from "../components/admin/AdminComponent";
+import LoginComponent from "../components/login/LoginComponent";
+import ProductsProvider from "../contexts/ProductsContext";
+import ProductNotSelectedComponent from "../components/products/ProductsNotSelectedComponent";
+import ProductDetailsComponent from "../components/products/ProductDetailsComponent";
 
 export default (
     <Routes>
         <Route path="/" element={<HomeComponent />} />
         <Route path="/about" element={<AboutComponent />} />
+        <Route path="/products" element={
+            <ProductsProvider>
+                <ProductsComponent />
+            </ProductsProvider>
+        }>
+            <Route path="" element={<ProductNotSelectedComponent />} />
+            <Route path=":productId" element={<ProductDetailsComponent />} />
+        </Route>
+        <Route path="/admin" element={<AdminComponent />} />
+        <Route path="/login" element={<LoginComponent />} />
         <Route path="*" element={<NoMatchComponent />} />
     </Routes>
 );
