9b73dbd1e18a1c433f163c78e0e042ef34766150
Manish Sharma <manishvbn@gmail.com>
2025-09-29 13:33:03 +0530

    Manage Product Component Added

---
+++ Day7/react-redux-app/src/components/products/ManageProductComponent.jsx
@@ -0,0 +1,10 @@
+
+const ManageProductComponent = () => {
+    return (
+        <div>
+            <h1>Add or Edit Product</h1>
+        </div>
+    );
+};
+
+export default ManageProductComponent;
